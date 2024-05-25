07a5a8aa4da735c062b1197675bc0aecb1a191ab
Truong Duy <truongduy.vithanhcity@gmail.com>
2024-05-26 00:12:24 +0700

    fix: sign-out function & avatar for user

--- src/components/SlideBar/components/FooterSideBar.tsx
+++ src/components/SlideBar/components/FooterSideBar.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 import { useAuth } from "@/providers/AuthContext";
+import { useRouter } from "next/navigation";
 import { FC, useEffect, useState } from "react";
 import { CiLogout } from "react-icons/ci";
 
 const FooterSideBar: FC = () => {
-  const { user } = useAuth();
+  const navigate = useRouter();
+
+  const { user, signOut } = useAuth();
 
   const [isClient, setIsClient] = useState(false);
 
@@ -18,17 +21,25 @@ const FooterSideBar: FC = () => {
     return null;
   }
 
+  const handleLogout = async () => {
+    await signOut();
+    navigate.replace("/auth/login");
+  };
+
   return (
     <div className="p-5 pt-7 flex items-center gap-2">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png"
+        src={
+          user?.user?.avatar ||
+          "https://cdn-icons-png.flaticon.com/512/9131/9131529.png"
+        }
         className="w-10"
       />
       <p className="flex flex-col flex-1 leading-5">
         <p>{user?.user.username}</p>
         <p className="text-xs text-gray-700">User of Kanban Zone app</p>
       </p>
-      <span className="cursor-pointer rotate-180">
+      <span className="cursor-pointer rotate-180" onClick={handleLogout}>
         <CiLogout size={25} />
       </span>
     </div>
